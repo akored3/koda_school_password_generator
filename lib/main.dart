e0c0ad2be9730d34ab61796f224c76363691c743
JIDE AKINBOLAJI <akinjideakinbolaji@outlook.com>
2024-03-14 12:04:25 +0100

    all done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:password_generator/bloc/password_bloc.dart';
-import 'package:password_generator/bloc/password_state.dart';
-import 'package:password_generator/service/password_generator.dart';
-import 'package:password_generator/views/password_generator_view.dart';
 import 'package:password_generator/views/welcome_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(ProviderScope(
+  runApp(const ProviderScope(
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Password Generator',
-      home: BlocProvider<PasswordGeneratorBloc>(
-        create: (context) => PasswordGeneratorBloc(PasswordGenerator()),
-        child: const Welcome(),
-      ),
+      home: Welcome(),
     ),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<PasswordGeneratorBloc, PasswordGeneratorState>(
-      builder: (context, state) {
-        if (state is PasswordGeneratorInitialState) {
-          return const Welcome();
-        } else if (state is PasswordGeneratorOnState) {
-          return const PasswordGeneratorView();
-        } else {
-          return const Scaffold(
-            body: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
-  }
 }
